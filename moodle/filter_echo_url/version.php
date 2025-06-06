debdffdcc309210ffdfeeca317f93b7bdd8a52c8
steven-kollo <steven.kollo.workspace@gmail.com>
2025-06-06 15:38:41 +0400

    tiny fix

--- moodle/filter_echo_url/version.php
+++ moodle/filter_echo_url/version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'filter_echo_url';
-$plugin->version = 20250604456;
+$plugin->version = 20250604457;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.1';
